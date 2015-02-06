eba13e8e4921d68614c1912afddf7acab8f48fae
George Li <georgejdli@gmail.com>
2015-02-05 16:10:24 -0800

    add built files

--- gulpfile.js
+++ gulpfile.js
@@ -115,7 +115,7 @@ gulp.task('watch', ['connect', 'serve'], function () {
 });
 
 //permissions error with  'images', so removed it for now
-gulp.task('build', ['jshint', 'html', 'fonts', 'extras'], function () {
+gulp.task('build', ['jshint', 'html', 'images','fonts', 'extras'], function () {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
